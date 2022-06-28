40b9c239debcd96a3151c46ef6a71cd72f9b7a3e
Tharinda Nimnajith Rajapaksha <tharindarajapakshe@gmail.com>
2022-06-28 16:55:52 +0530

    init

---
+++ client/clock.js
@@ -0,0 +1,35 @@
+Meteor.setInterval(function () {
+  Session.set('time', new Date());
+}, 1000);
+
+Template.body.helpers({
+
+  hours: _.range(0, 12),
+
+  degrees: function () {
+    return 30 * this;
+  },
+
+  handData: function () {
+    const time = Session.get('time') || new Date();
+    return {
+      hourDegrees: time.getHours() * 30,
+      minuteDegrees: time.getMinutes() * 6,
+      secondDegrees: time.getSeconds() * 6
+    };
+  },
+
+  radial: function (angleDegrees,
+                    startFraction,
+                    endFraction) {
+    const r = 100;
+    const radians = (angleDegrees - 90) / 180 * Math.PI;
+
+    return {
+      x1: r * startFraction * Math.cos(radians),
+      y1: r * startFraction * Math.sin(radians),
+      x2: r * endFraction * Math.cos(radians),
+      y2: r * endFraction * Math.sin(radians)
+    };
+  }
+});
